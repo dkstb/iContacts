d4721541ed482cf8f8d64db96a6df285276f805d
godongsori <godongsori@gmail.com>
2013-11-20 19:37:27 +0900

    로그인 테스트 완성...

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
 			if (data.result == 'login') {
 				$('#header_login').attr('style', 'display:none');
 				$('#header_signup').attr('style', 'display:none');
-				$('#header_account').attr('style', 'display:block');
+				$('#header_account').removeAttr('style');
 				$('#account_name').text(data.users.name + ' 님');
 			} else if (data.result == 'logout'){
 				$('#header_account').attr('style', 'display:none');
-				$('#header_login').attr('style', 'display:block');
-				$('#header_signup').attr('style', 'display:block');
+				$('#header_login').removeAttr();
+				$('#header_signup').removeAttr();
 			} else {
 				alert('회원님의 로그인 정보가 DB에 존재하지 않습니다. 브라우저를 재실행 하시고, 다시 가입해주세요.');
 			}

559cbd8f0d82b77053f83c40989b72425055279d
bestjish <bestjish@naver.com>
2013-11-21 22:06:13 +0900

    주소록 페이지 삭제 완성

--- src/main/webapp/resources/js/delete_contact.js
+++ src/main/webapp/resources/js/delete_contact.js
@@ -1,22 +1,31 @@
 $(document).ready(function() {
-
-	$(document).on('click', '#delete_contact', function(){
+	
+	$(document).on('click',  '#delete_contact', function(){
 		// ajax 전송
-		$.ajax('/bitschool/ajax/deleteContact.contact', {
-
-			type : 'POST',
-			dataType : 'json',
-			data : {
-				id : $('#contact_id').val()
-			},
-			success : function(data) {
-				if (data.result == 'success') {
-					alert($('#contact_name').val() + '님이 삭제되었습니다.');
+		
+		var result = confirm('정말로 삭제하겠습니까?');
+		if(result){
+			$.ajax('/bitschool/ajax/deleteContact.contact', {
+	
+				type : 'POST',
+				dataType : 'json',
+				data : {
+					id : $('#contact_id').val()
+				},
+				success : function(data) {
+					
+					alert('정상적으로 삭제되었습니다.');
 					location.reload();
-				} else {
-					alert('주소록 삭제가 실패했습니다.');
+					
 				}
-			}
-		});
+			});
+			
+			
+		}else{
+			alert($('삭제가 취소되었습니다.'));
+
+		}
+		
 	});
-});
+		
+	});

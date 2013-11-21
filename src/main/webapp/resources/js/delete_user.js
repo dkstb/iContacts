bf39e48a0ad42adeb1754a3a842cfdffaf4a56bd
chan <chan@chan-PC>
2013-11-21 21:07:30 +0900

    user 계정 삭제 확인창 추가

--- src/main/webapp/resources/js/delete_user.js
+++ src/main/webapp/resources/js/delete_user.js
@@ -10,8 +10,13 @@ $(document).ready(function() {
 			},
 			success : function(data) {
 				if (data.result == 'success') {
+					var result = confirm($('#name').val()+'을 정말로 삭제하겠습니까?'); 
+					if(result){
 					alert($('#name').val() + '님이 삭제되었습니다.');
 					$(location).attr('href','index.html');
+					}else{
+						alert('삭제가 실패했습니다.')
+					}
 				} else {
 					alert('삭제가 실패했습니다.');
 				}

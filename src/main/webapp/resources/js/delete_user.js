69fe45a63129b625aaf54fc07c966d1e2f82203b
chan <chan@chan-PC>
2013-11-21 21:55:52 +0900

    진짜 회원탈퇴 확인창 완료염

--- src/main/webapp/resources/js/delete_user.js
+++ src/main/webapp/resources/js/delete_user.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
 
 	$('#delete').click(function() {
-		// ajax 전송
-		$.ajax('/bitschool/ajax/deleteUser.contact', {
-			type : 'POST',
-			dataType : 'json',
-			data : {
-				id : $('#user_id').val()
-			},
-			success : function(data) {
-				if (data.result == 'success') {
-					var result = confirm($('#name').val()+'을 정말로 삭제하겠습니까?'); 
-					if(result){
-					alert($('#name').val() + '님이 삭제되었습니다.');
-					$(location).attr('href','index.html');
-					}else{
-						alert('삭제가 실패했습니다.')
+		var result = confirm($('#name').val() + '을 정말로 삭제하겠습니까?');
+		if (result) {
+			// ajax 전송
+			$.ajax('/bitschool/ajax/deleteUser.contact', {
+				type : 'POST',
+				dataType : 'json',
+				data : {
+					id : $('#user_id').val()
+				},
+				success : function(data) {
+
+					if (data.result == 'success') {
+						alert($('#name').val() + '님이 삭제되었습니다.');
+						$(location).attr('href', 'index.html');
+
+					} else {
+						alert('삭제가 실패했습니다.');
 					}
-				} else {
-					alert('삭제가 실패했습니다.');
 				}
-			}
-		});
+			});
+
+		} else {
+			alert('삭제를 취소했습니다.')
+
+		}
+
 	});
 });
